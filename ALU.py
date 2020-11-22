2b7f931fcf0917e5f0399ba3f6f1d9393ae8f932
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-11-22 01:14:09 -0500

    Colores y estilos

--- ALU.py
+++ ALU.py
@@ -1,4 +1,5 @@
 import os
+import styles as st
 import random as rnd
 from exceptions import *
 
@@ -15,13 +16,10 @@ stack = {
     "05h": "0b0",
     "06h": "0b0"
 }
-# SF = Resultado negativo
-# ZF = Resultado cero 
-# CF = Acarreo
 flags = {
-    "SF": '0b0',
-    "ZF": '0b0',
-    "CF": '0b0'
+    "SF": '0b0',  # SF = Resultado negativo
+    "ZF": '0b0',  # ZF = Resultado cero
+    "CF": '0b0'   # CF = Acarreo
 }
 
 
@@ -56,6 +54,7 @@ def operacion_div(params):
     else:
         raise ParametrosIncorrectosExcepcion("DIV", 2)
 
+
 def operacion_sub(params):
     """
     Metodo que resta 2 posiciones de memoria y
@@ -69,6 +68,7 @@ def operacion_sub(params):
         stack[params[0]] = res
     else:
         raise ParametrosIncorrectosExcepcion("SUB", 2)
+
 
 def operacion_add(params):
     """
@@ -84,6 +84,7 @@ def operacion_add(params):
     else:
         raise ParametrosIncorrectosExcepcion("ADD", 2)
 
+
 def operacion_neg(params):
     """
     Metodo que cambia el signo de una direccion
@@ -97,19 +98,21 @@ def operacion_neg(params):
     else:
         raise ParametrosIncorrectosExcepcion("NEG", 1)
 
+
 def operacion_and(params):
     """
     Metodo que realiza la operación AND entre 2 posiciones
     de memoria y guarda el resultado en la primera
     """
     if len(params) == 2:
-         a = obtener_dir(params[0])
-         b = obtener_dir(params[1])
+        a = obtener_dir(params[0])
+        b = obtener_dir(params[1])
 
-         res = to_bin(a & b)
-         stack[params[0]] = res
+        res = to_bin(a & b)
+        stack[params[0]] = res
     else:
         raise ParametrosIncorrectosExcepcion("AND", 2)
+
 
 def operacion_or(params):
     """
@@ -117,13 +120,14 @@ def operacion_or(params):
     de memoria y guarda el resultado en la primera
     """
     if len(params) == 2:
-         a = obtener_dir(params[0])
-         b = obtener_dir(params[1])
+        a = obtener_dir(params[0])
+        b = obtener_dir(params[1])
 
-         res = to_bin(a | b)
-         stack[params[0]] = res
+        res = to_bin(a | b)
+        stack[params[0]] = res
     else:
         raise ParametrosIncorrectosExcepcion("OR", 2)
+
 
 def reset(params):
     """
@@ -167,7 +171,8 @@ def procesar(entrada):
         parametros = intrucciones[1:]
         operacion_func(parametros)
     except Exception as err:
-        print("Error: " + str(err) + "\n")
+        print(st.ROJO + st.NEGRITA + "Error: " +
+              st.NORMAL + st.ROJO + str(err) + st.RESET + "\n")
 
 
 def generar_stack():
@@ -200,25 +205,28 @@ def mostrar_tabla():
     Metodo que muestra la informacion del stack y los flags
     en formato de tabla
     """
-    v = "│"
-    h = "─"
-    c = "×"
-    s = " "
+    _border_format = st.CIAN + "%s" + st.RESET
+    v = _border_format % "│"
+    h = _border_format % "─"
+    c = _border_format % "×"
+    s = _border_format % " "
 
     fila_inicio = (s * 6) + c + (h * 65) + c + (h * 32) + c + "\n"
-    fila_titulos = (s * 6) + v + (s * 30) + "Stack" + (s * 30) + \
-        v + (s * 14) + "Flags" + (s * 13) + v + "\n"
+    fila_titulos = (s * 6) + v + (s * 30) + st.VERDE + st.NEGRITA + \
+        "Stack" + st.RESET + (s * 30) + v + (s * 14) + \
+        st.VERDE + st.NEGRITA + "Flags" + st.RESET + (s * 13) + v + "\n"
 
     fila_separador = c + (h * 5) + c + (((h * 10) + c) * 9) + "\n"
-    fila_dir = v + " Dir " + v + \
+    fila_dir = v + st.VERDE + st.NEGRITA + " Dir " + st.RESET + v + \
         (("   %s    " + v) * 6) + \
         (("    %s    " + v) * 3) + "\n"
-    file_val = v + " Val " + v + (" %s " + v) * 9 + "\n"
+    file_val = v + st.VERDE + st.NEGRITA + \
+        " Val " + st.RESET + v + (" %s " + v) * 9 + "\n"
 
     dirs = list()
     vals = list()
     for k, v in dict(stack, **flags).items():
-        dirs.append(k)
+        dirs.append(st.NEGRITA + k + st.RESET)
         vals.append(v[2:])
 
     tabla = fila_inicio + fila_titulos + fila_separador + \
@@ -232,12 +240,13 @@ def mostrar_operaciones():
     """
     Metodo que muestra las operaciones del programa
     """
-    print("OPERACIONES:",end="")
+    print(st.NEGRITA + st.AMARILLO + "OPERACIONES:", end=st.RESET)
     ops = operaciones
     for i in range(len(ops)):
         if i % 3 == 0:
             print()
-        print(ops[i] + "\t" + to_bin(i), end="\t")
+        print(st.NEGRITA + st.AZUL + ops[i] +
+              st.RESET + "\t" + to_bin(i), end="\t")
     print("\n")
 
 
@@ -248,7 +257,8 @@ def menu():
     mostrar_operaciones()
     while True:
         mostrar_tabla()
-        print("ALU > ", end="")
+        prompt = st.MORADO + st.NEGRITA + "ALU > "
+        print(prompt, end=st.RESET)
         instruccion = input().lower().strip()
         print()
         procesar(instruccion)
---
+++ styles.py
@@ -0,0 +1,15 @@
+_style = "\033[%dm"
+
+NORMAL = _style % 0
+NEGRITA = _style % 1
+
+NEGRO = _style % 30
+ROJO = _style % 31
+VERDE = _style % 32
+AMARILLO = _style % 33
+AZUL = _style % 34
+MORADO = _style % 35
+CIAN = _style % 36
+BLANCO = _style % 37
+
+RESET = NORMAL + BLANCO
